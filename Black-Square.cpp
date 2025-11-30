9c39e37bf6f9aca0ee7385acd2dde62b056628dc
Mariem Hossam <marim.h2004@hotmail.com>
2025-11-30 17:03:02 +0200

    solved

--- Black-Square.cpp
+++ Black-Square.cpp
@@ -1,6 +1,26 @@
 // https://codeforces.com/contest/431/problem/A
+
 #include <iostream>
 using namespace std;
+
 int main()
 {
+    int arr[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+    }
+
+    string s;
+    cin >> s;
+
+    int total = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        int index = s[i] - '1';
+        total += arr[index];
+    }
+
+    cout << total;
 }
